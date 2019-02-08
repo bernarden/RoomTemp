a25d94e6445bcaa3cb7ae090946a80e20364fd3b
Victor Usoltsev <bernarden@mail.ru>
2019-02-09 00:37:49 +1300

    Adds Range selection buttons.

--- Server/RoomTemp/ClientApp/src/App.tsx
+++ Server/RoomTemp/ClientApp/src/App.tsx
@@ -1,24 +1,59 @@
 import * as React from 'react';
+import * as classnames from 'classnames';
+
 import './App.css';
 import logo from './logo.svg';
 import TemperatureChart from './components/temperatureChart/TemperatureChart';
+import { TempReadingRange } from './api/tempReadingRange';
 
-class App extends React.Component {
-  constructor(props: any) {
-    super(props);
-  }
-
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to your Temperature Reading</h1>
-        </header>
-
-        <TemperatureChart />
-      </div>
-    );
-  }
+interface IState {
+    selectedRange: TempReadingRange
 }
+
+class App extends React.Component<{}, IState> {
+    constructor(props: any) {
+        super(props);
+        this.state = { selectedRange: TempReadingRange.Week };
+    }
+
+    public render() {
+        const rangeButtons = this.renderRangeButtons();
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Temperature Readings</h1>
+                </header>
+
+                {rangeButtons}
+
+                <TemperatureChart/>
+            </div>
+        );
+    }
+
+    private renderRangeButtons() {
+        return (
+            <div className="btn-group btn-group-toggle" style={{paddingBottom:'10px'}} data-toggle="buttons">
+                {this.renderRangeButton(TempReadingRange.Day, 'Day')}
+                {this.renderRangeButton(TempReadingRange.Week, 'Week')}
+            </div>);
+    }
+
+    private renderRangeButton(range: TempReadingRange, rangeDisplayName: string) {
+        const classes = classnames('btn btn-secondary', { 'active': this.state.selectedRange === range});
+        return (
+            <label className={classes}>
+                <input type="radio" name="options" id="option1" onClick={this.handleRangeSelectionClick(range)}/>
+                {rangeDisplayName}
+            </label>
+        );
+    }
+
+    private handleRangeSelectionClick = (range: TempReadingRange) => (e: React.SyntheticEvent<any>): void => {
+        this.setState({ selectedRange: range });
+    };
+}
+
+
 export default App;
---
+++ Server/RoomTemp/ClientApp/src/api/tempReadingRange.ts
@@ -0,0 +1,7 @@
+export enum TempReadingRange {
+    Unspecified = 0,
+    Hour = 1,
+    Day = 2,
+    Week = 3,
+    Month = 4
+}
