0af4018e60e55b1ba2a68b480da14e2353dd0c00
Victor Usoltsev <bernarden@mail.ru>
2019-12-25 22:12:56 +1300

    Adds missing index for TempReading table.

---
+++ Server/RoomTemp.Data/Migrations/20191225090848_AddsMissingIndexForTempReadingTable.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RoomTemp.Data.Migrations
+{
+    public partial class AddsMissingIndexForTempReadingTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+IF NOT EXISTS (SELECT name FROM sysindexes WHERE name = 'IX_TempReading_SensorId_DeviceId_LocationId_TakenAt')
+BEGIN
+	CREATE NONCLUSTERED INDEX [IX_TempReading_SensorId_DeviceId_LocationId_TakenAt] ON [dbo].[TempReading]
+	(
+		[SensorId] ASC,
+		[DeviceId] ASC,
+		[LocationId] ASC,
+		[TakenAt] ASC
+	)
+	INCLUDE([Temperature]) WITH (STATISTICS_NORECOMPUTE = OFF, DROP_EXISTING = OFF, ONLINE = OFF) ON [PRIMARY]
+END");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            if (migrationBuilder.IsSqlServer())
+            {
+                migrationBuilder.Sql(@"
+IF EXISTS (SELECT name FROM sysindexes WHERE name = 'IX_TempReading_SensorId_DeviceId_LocationId_TakenAt')
+BEGIN
+    DROP INDEX [IX_TempReading_SensorId_DeviceId_LocationId_TakenAt] ON [dbo].[TempReading];
+END");
+            }
+        }
+    }
+}
